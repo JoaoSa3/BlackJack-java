d59e83c487a7d2d3c19e314ea9613a75fa4c60a7
JoaoSa3 <joaosa330@gmail.com>
2026-02-09 22:11:51 +0000

    Delete iso-2025-proyect-nepal-national-team-main directory

--- iso-2025-proyect-nepal-national-team-main/src/com/nepalnationalteam/blackjack/Main.java
+++
@@ -1,7 +0,0 @@
-package com.nepalnationalteam.blackjack;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new StartScreenFrame().setVisible(true));
-    }
-}
